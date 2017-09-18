0d3b9073f9d949bb861eeec335c5190ab4706232
Nguyen Tran <nguyentran@Nguyens-MacBook-Pro.local>
2017-09-18 12:08:33 -0700

    HW2 first commit

---
+++ Homework2/README.md
@@ -0,0 +1,15 @@
+CPSC 473 - Web Front-End Engineering - Fall 2017
+Homework 1, due September 11
+Print this sheet, follow the instructions below, and give it to the professor in class by the deadline.
+In this assignment, you will work through Chapters 3 and 4 of the textbook, upload the result to GitHub, and play some games designed to teach you CSS.
+Complete the following:
+1.	Work through Chapters 3 and 4, copying in files from front-end-dev-resources/ and updating files as necessary.
+2.	Confirm that your result matches the Ottergram page shown in Figure 4.27 at the end of Chapter 4.
+3.	Create a screenshots/ directory in your repository.
+4.	Play the following games through to the final level:
+a.	CSS Diner
+b.	One of Flexbox Froggy or Flexbox Defense
+c.	Grid Garden
+5.	Take screenshots of the completed final level of each game and place them in the screenshots/ directory.
+6.	Push the contents of your ottergram/ directory into a new public GitHub repository.
+
---
+++ Homework2/ottergram/scripts/main.js
@@ -0,0 +1,89 @@
+var DETAIL_IMAGE_SELECTOR = '[data-image-role="target"]';
+var DETAIL_TITLE_SELECTOR = '[data-image-role="title"]';
+var THUMBNAIL_LINK_SELECTOR = '[data-image-role="trigger"]';
+var randomThumb;
+var usedImg;
+var tacocaturl = "https://img0.etsystatic.com/135/0/9793565/il_340x270.1006989508_atrg.jpg";
+// Set Details with image URL and Title Text
+
+function setDetails(imageURL, titleText) {
+  'use strict';
+  var detailImage = document.querySelector(DETAIL_IMAGE_SELECTOR);
+  detailImage.setAttribute('src', imageURL);
+
+  var detailTitle = document.querySelector(DETAIL_TITLE_SELECTOR);
+  detailTitle.textContent = titleText;
+
+}
+
+// Get images from Thumbnail
+function imageFromThumb(thumbnail) {
+  'use strict';
+  return thumbnail.getAttribute('data-image-url');
+}
+
+// Get titles from Thumbnail
+function titleFromThumb(thumbnail) {
+  'use strict';
+  return thumbnail.getAttribute('data-image-title');
+}
+
+// Set Details from Thumb
+function setDetailsFromThumb(thumbnail) {
+  'use strict';
+  setDetails(imageFromThumb(thumbnail), titleFromThumb(thumbnail));
+}
+
+
+//Function generate random Number
+function randomNumber() {
+  var thumbnails = getThumbnailsArray();
+  var length = thumbnails.length;
+  var randomNumb = Math.floor(Math.random() * length);
+  return randomNumb;
+}
+
+//Function set Thumbnails Tacocat in random place
+function setRandomTacocat() {
+  var thumbnails = getThumbnailsArray();
+  var num = randomNumber();
+  randomThumb = thumbnails[num];
+  usedImg = randomThumb.getAttribute('data-image-url');
+  randomThumb.setAttribute('data-image-url', tacocaturl);
+}
+
+
+// Add thumbnail click, set detail, and prevent default
+function addThumbClickHandler(thumb) {
+  'use strict';
+  thumb.addEventListener('click', function(event) {
+    event.preventDefault();
+
+    var imgURL = imageFromThumb(thumb);
+    if(imgURL = tacocaturl){
+      randomThumb.setAttribute('data-image-url', usedImg);
+      setRandomTacocat();
+    }
+
+    setDetailsFromThumb(thumb);
+  });
+}
+
+// Get Thumbnail Array
+function getThumbnailsArray() {
+  'use strict';
+  var thumbnails = document.querySelectorAll(THUMBNAIL_LINK_SELECTOR);
+  var thumbnailArray = [].slice.call(thumbnails);
+  return thumbnailArray;
+}
+
+// initializeEvents
+function initializeEvents() {
+  'use strict';
+  setRandomTacocat();
+  var thumbnails = getThumbnailsArray();
+  thumbnails.forEach(addThumbClickHandler);
+
+}
+
+initializeEvents();
